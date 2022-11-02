499ddf6fcce329d692310fb581d8225129efbb71
SamuelOliveiraSB <samuel.oliveira@sptech.school>
2022-11-02 01:34:55 -0300

    telas iniciais e interação

--- new/inter.js
+++ new/inter.js
@@ -1,16 +1,65 @@
-var botaologin = document.querySelector('#blogin')
-var botaocadastro = document.querySelector('#bcadastro')
 var telalogin = document.querySelector('#telalogin')
 var opcoesdelogin = document.querySelector('#opclog')
 var opcoesdecadastro = document.querySelector('#opccad')
 
-botaologin.addEventListener('click',()=>{
-    console.log('foi')
+//-------------------------------------------------- cadastro e login
+function irlogin(){
     telalogin.style.display = 'flex'
     opcoesdelogin.style.display = 'flex'
+}
+
+function ircadastro(){
+    telalogin.style.display = 'flex'
+    opcoesdelogin.style.display = 'none'
+    opcoesdecadastro.style.display = 'flex'
+}
+
+//--- de login pra home
+
+
+function entrar(){
+    window.location.href = "home.html";
+}
+
+
+//------------------------------------------------- animacão pra trocar de carro
+
+var divpai = document.querySelector('.divdown')
+var divsfilhas = document.querySelectorAll('.divdown div')
+
+
+divsfilhas.forEach((div, index) => {
+
+    div.addEventListener('click', () => {
+
+        divpai.querySelector('.animate').classList.remove('animate')
+        div.classList.add('animate')
+
+        if (index == 0) {
+            imagemdocarro.src = 'assets/cars/Nissan_Skyline_GT-R_VSpec_2_29.webp'
+            nomecarro.innerHTML=`nissan gtr r34`
+            motor.innerHTML=``
+            potencia.innerHTML=``
+            torque.innerHTML=``
+        } else if (index == 1) {
+            imagemdocarro.src = 'assets/cars/r35v2.png'
+            nomecarro.innerHTML=`nissan gtr r35`
+            motor.innerHTML=``
+            potencia.innerHTML=``
+            torque.innerHTML=``
+        }
+        else if (index == 2) {
+            imagemdocarro.src = 'assets/cars/r35v2.png'
+            nomecarro.innerHTML=`toyota supra mk4`
+            motor.innerHTML=``
+            potencia.innerHTML=``
+            torque.innerHTML=``
+        }
+
+        
+
+    })
+
+
 })
 
-botaocadastro.addEventListener('click',()=>{
-    telalogin.style.display = 'flex'
-    opcoesdecadastro.style.display = 'flex'
-})
